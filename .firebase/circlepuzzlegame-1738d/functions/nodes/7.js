b233f30fcbaaf90b262d8ad48881606cae6d41a3
hmacht <32553134+hmacht@users.noreply.github.com>
2024-05-10 15:48:40 -0400

    ads.txt file added

--- .firebase/circlepuzzlegame-1738d/functions/nodes/7.js
+++ .firebase/circlepuzzlegame-1738d/functions/nodes/7.js
@@ -3,6 +3,6 @@
 export const index = 7;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/gameboard/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/7.CTtXmGwS.js","_app/immutable/chunks/scheduler.Dj2J5mG2.js","_app/immutable/chunks/index.BbVivFmB.js","_app/immutable/chunks/Stats.9q4SY6Jd.js","_app/immutable/chunks/index.DHaQdJMq.js"];
+export const imports = ["_app/immutable/nodes/7.BeMkha2g.js","_app/immutable/chunks/scheduler.Dj2J5mG2.js","_app/immutable/chunks/index.BbVivFmB.js","_app/immutable/chunks/Stats.9q4SY6Jd.js","_app/immutable/chunks/index.DHaQdJMq.js"];
 export const stylesheets = ["_app/immutable/assets/7.DAaRcdKi.css","_app/immutable/assets/Stats.Bsj0LaHY.css"];
 export const fonts = [];
--- .firebase/circlepuzzlegame-1738d/hosting/_app/immutable/entry/start.Bc9Q2PPq.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.lwHW2PhG.js";export{t as start};
