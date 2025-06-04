909f9569080dee6c0389f13a9b29907f04e5fb1f
hmacht <32553134+hmacht@users.noreply.github.com>
2025-06-04 01:31:53 -0400

    env vars for firebase

--- src/lib/firebase.client.ts
+++ src/lib/firebase.client.ts
@@ -25,13 +25,13 @@ export let realtimeDb: Database;
 // TODO: Stop being lazy and put these away in ENV
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4qZNCNnlVihIZbdgtBB82R48AIC0bqG4",
-  authDomain: "circlepuzzlegame-1738d.firebaseapp.com",
-  databaseURL: "https://circlepuzzlegame-1738d-default-rtdb.firebaseio.com",
-  projectId: "circlepuzzlegame-1738d",
-  storageBucket: "circlepuzzlegame-1738d.firebasestorage.app",
-  messagingSenderId: "62490740969",
-  appId: "1:62490740969:web:719c6ea78f8148089f4878"
+  apiKey: PUBLIC_FIREBASE_API_KEY,
+  authDomain: PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: PUBLIC_FIREBASE_APP_ID
 };
 
 export const initializeFirebase = () => {
